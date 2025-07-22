aa57389748e8b2a79765c2f47b2483f3fb6c9b31
rahel yabebal <rahelyabebal@gmail.com>
2025-07-22 17:14:24 +0300

    added .env file and moved connect to mongo

--- Delivery/main.go
+++ Delivery/main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"context"
+	"log"
 	"os"
 	Controllers "task_manager/Delivery/Controllers"
 	routers "task_manager/Delivery/routers"
 	Infrastructure "task_manager/Infrastructure"
 	Repositories "task_manager/Repositories"
 	Usecases "task_manager/Usecases"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	// Load environment variables from .env if present
+	_ = godotenv.Load()
+
 	// MongoDB connection
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
 		uri = "mongodb://localhost:27017" // default fallback
 	}
-	client := Infrastructure.ConnectMongo(uri)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
 	db := client.Database("task_manager")
 
 	// Repositories
@@ -45,4 +63,3 @@ func main() {
 	router := routers.SetupRouter(userController, taskController, jwtSecret)
 	router.Run()
 }
-
